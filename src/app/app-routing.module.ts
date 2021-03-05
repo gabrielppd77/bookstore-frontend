960e0d69a941d172591beb960c9eed65d196a7ab
gabrielppd77 <gabrielppd77@outlook.com>
2021-03-04 21:30:52 -0300

    Delete livro implements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './components/views/home/home.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 import { LivroUpdateComponent } from './components/views/livro/livro-update/livro-update.component';
+import { LivroDeleteComponent } from './components/views/livro/livro-delete/livro-delete.component';
 
 const routes: Routes = [
   {
@@ -41,6 +42,10 @@ const routes: Routes = [
   {
     path: 'categorias/:id_cat/livros/:id/update',
     component: LivroUpdateComponent
+  },
+  {
+    path: 'categorias/:id_cat/livros/:id/delete',
+    component: LivroDeleteComponent
   }
 
 ];
